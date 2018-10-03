18666f557a53c7e02412350fb2893a19ff99a6fd
olyak2005 <olyak2005@gmail.com>
2018-10-04 02:09:33 +0300

    Add maven project.

---
+++ src/main/java/edu/softserve/com/WishListMaven/App.java
@@ -0,0 +1,13 @@
+package edu.softserve.com.WishListMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
