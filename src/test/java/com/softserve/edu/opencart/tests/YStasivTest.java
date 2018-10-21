e9b2f019c9054ae47faea99298b7ab56a45bfae6
Yurii Stasiv <Yurastasiv@hotmail.com>
2018-10-21 21:49:50 +0300

    Add SmokeTest

--- src/test/java/com/softserve/edu/opencart/tests/YStasivTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/YStasivTest.java
@@ -11,6 +11,33 @@ import com.softserve.edu.opencart.pages.HomePage;
 import com.softserve.edu.opencart.tools.TestRunner;
 
 public class YStasivTest extends TestRunner {
+	
+	@Test//TODO УТОЧНИТИ ЯК ПОПРАВИТИ ЦЮ ФІГНЮ!!!
+	public void SmokeTestOpenCart() {
+		SoftAssert softAssert = new SoftAssert();
+//Precondition: Load Application
+		HomePage homePage = loadApplication();
+		delayExecution(1000); //ForDemonstration
+		
+//Steps: Add product to cart
+		homePage.getProductsListComponentTitleText();
+		delayExecution(1000); //ForDemonstration
+
+//Check if page contains curent data
+		softAssert.assertEquals(homePage.getProductsListComponentTitleText(),homePage.EXPECT_PRODUCT_LIST_TITLE);
+		delayExecution(1000); //ForDemonstration
+		Assert.assertEquals(homePage.getCartSum(), 0 & homePage.getCartAmount(), 0);
+		
+		//TODO ЗАПИТАТИ ЗА СОФТ АСЕРТ, ЧОГО ВІН НЕ КИДАЄ ЕКСПШИН, ЧИ ВІН І НЕ МАЄ КИДАТИ, І ПОЧИТАТИ ПРО НЬОГО ТРОХИ ШОСЬ
+		//плюс уточнити за футер
+		
+		delayExecution(1000); //ForDemonstration
+		Assert.assertTrue(homePage.gotoLogin().getLastBreadcrumbText().contains("Login"));
+		
+		//TODO СПИТАТИ ЧИ КРАЩЕ ЗАЮЗАТИ ISDISPLAYED
+		
+		delayExecution(2000); //ForDemonstration
+	}//TODO Логер + репортер
 
 	@DataProvider // (parallel = true)
 	public Object[][] currenciesType() {
@@ -28,9 +55,11 @@ public class YStasivTest extends TestRunner {
 //Precondition: Load Application
 		HomePage homePage = loadApplication();
 		delayExecution(1000); //ForDemonstration
+		
 //Steps: Choose currencies and go to Shopping Cart
 		homePage.chooseCurrency(currency).clickShoppingCart();;
 		delayExecution(1000); //ForDemonstration
+		
 //Check if user see Empty Cart Page
 		Assert.assertEquals(gotoEmptyShoppingCartPage().getEmptyCartText(),
 				gotoEmptyShoppingCartPage().EXPECT_EMPTY_CART_TEXT);
@@ -54,18 +83,23 @@ public class YStasivTest extends TestRunner {
 //Precondition: Load Application
         HomePage homePage = loadApplication();
         delayExecution(1000); //ForDemonstration
+        
 //Steps: Add product to cart
         HomeMessagePage homeMessagePage = homePage.putToCartProductByPartialName(partialProductName);
         delayExecution(1000); //ForDemonstration
-        //
+
 //Check if AlertMessage contains current text
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertTrue(homeMessagePage.getAlertMessageText().contains(String.format(homeMessagePage.EXPECTED_MESSAGE_CART, partialProductName)));
         //TODO УТОЧНИТИ ЧИ СОФТ АСЕРТ АКТУАЛЬНИЙ ТУТ
         delayExecution(1000); //ForDemonstration
+        
 // Go to ShoppingCart and check if goods was added
         homePage.clickShoppingCart();
         Assert.assertTrue(gotoProductsListCartComponent().getProductsCartNameList().contains(partialProductName));
         delayExecution(2000); //ForDemonstration
     }//TODO Логер + репортер
+	
+
+	
 }
