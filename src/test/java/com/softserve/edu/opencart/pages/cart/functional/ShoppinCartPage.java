5778468ee5044b829a9a668863436868ff7221a4
Yurii Stasiv <Yurastasiv@hotmail.com>
2018-10-22 01:11:41 +0300

    Add expected alert

--- src/test/java/com/softserve/edu/opencart/pages/cart/functional/ShoppinCartPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/cart/functional/ShoppinCartPage.java
@@ -5,14 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.softserve.edu.opencart.pages.AStatusBarComponent;
+import com.softserve.edu.opencart.pages.HomeMessagePage;
 import com.softserve.edu.opencart.tools.RegexUtils;
 
 public class ShoppinCartPage extends AStatusBarComponent {
 
-	private WebElement arrayList;
-	private WebElement quantityField;
-	private WebElement updateButton;
-	private WebElement removeButton;
+	private ProductsListCartComponent productsCartListComponent;
 //	private WebElement cuponCodeField;
 //	private WebElement applyCuponCpdeButton;
 //	private WebElement estimateShippingAndTaxes;
@@ -35,32 +33,13 @@ public class ShoppinCartPage extends AStatusBarComponent {
 	private void initEmptyCartPageText() {
 //		arrayList
 		// checkEmptyCartPage = driver.findElement(By.cssSelector("#content p"));
-		quantityField = driver.findElement(By.xpath("//div[contains(@class,'table')]//a[text()='MacBook']/../..//div[contains(@class, 'input-group')]//input"));
-		updateButton = driver.findElement(By.xpath("//div[contains(@class,'table')]//a[text()='MacBook']/../..//i[contains(@class,'fa-refresh')]"));
-		removeButton = driver.findElement(By.xpath("//div[contains(@class,'table')]//a[text()='MacBook']/../..//i[contains(@class,'fa-times-circle')]"));
 		totalPrice = driver.findElement(By.xpath("//td/strong[text()='Total:']/../../td[contains(text() , '')]"));
 //		continueShoppingButton = driver.findElement(By.cssSelector("a.btn.btn-default"));
 		checkoutButton = driver.findElement(By.cssSelector("a.btn.btn-primary"));
 	}
 
 	//AtomicOperation
-	
-	// QuantityField
-	public WebElement getQuantityField() {return quantityField;}
-	public String setQuantityField() {return getQuantityField().getAttribute(TAG_ATTRIBUTE_VALUE);}
-	public void setQuantityField(String text) {getQuantityField().sendKeys(text);}
-	public void clearQuantityField() {getQuantityField().clear();}
-	public void clickQuantityField() {getQuantityField().click();}
-
-	//UpdateButton
-	public WebElement getUpdateButton() {return updateButton;}
-	public String getUpdateButtonText() {return getUpdateButton().getAttribute(TAG_ATTRIBUTE_VALUE);}
-	public void clickUpdateButton() {getUpdateButton().click();}
-	
-	//RemoveButton
-	public WebElement getRemoveButton() {return removeButton;}
-	public String getRemoveButtonText() {return getRemoveButton().getAttribute(TAG_ATTRIBUTE_VALUE);}
-	public void clickRemoveButton() {getRemoveButton().click();}
+	private ProductsListCartComponent getProductsCartListComponent() {return productsCartListComponent;}
 	
 	//TotalPrice
 	public WebElement getTotalPrice() {return totalPrice;}
@@ -72,5 +51,11 @@ public class ShoppinCartPage extends AStatusBarComponent {
 	public void clickCheckouteButton() {getCheckoutButton().click();}
 
 	// Business Logic
+	public ShoppingCartMessagePage updateProductQuantityByPartialName(String partialProductName) {
+		getProductsCartListComponent()
+			.updateProductCartByPartialName(partialProductName);
+        return new ShoppingCartMessagePage(driver); 
+    }
+	
 
 }
