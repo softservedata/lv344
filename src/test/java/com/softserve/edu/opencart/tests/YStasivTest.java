dea4a704468770be606389eec20c121c95be3a52
Yurii Stasiv <Yurastasiv@hotmail.com>
2018-10-21 20:23:39 +0300

    ADD SOFT ASSERT

--- src/test/java/com/softserve/edu/opencart/tests/YStasivTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/YStasivTest.java
@@ -3,6 +3,7 @@ package com.softserve.edu.opencart.tests;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.softserve.edu.opencart.data.Currencies;
 import com.softserve.edu.opencart.pages.HomeMessagePage;
@@ -34,6 +35,7 @@ public class YStasivTest extends TestRunner {
 		Assert.assertEquals(gotoEmptyShoppingCartPage().getEmptyCartText(),
 				gotoEmptyShoppingCartPage().EXPECT_EMPTY_CART_TEXT);
 		delayExecution(2000); //ForDemonstration
+//TODO ПОМІНЯТИ НАЗАД НА ДОЛАР
 	}//TODO Логер + репортер
 	
 	
@@ -57,16 +59,13 @@ public class YStasivTest extends TestRunner {
         delayExecution(1000); //ForDemonstration
         //
 //Check if AlertMessage contains current text
-        Assert.assertTrue(homeMessagePage.getAlertMessageText().contains(String.format(homeMessagePage.EXPECTED_MESSAGE_CART, partialProductName)));
-        
-        //TODO SoftAssert
-        
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(homeMessagePage.getAlertMessageText().contains(String.format(homeMessagePage.EXPECTED_MESSAGE_CART, partialProductName)));
+        //TODO УТОЧНИТИ ЧИ СОФТ АСЕРТ АКТУАЛЬНИЙ ТУТ
         delayExecution(1000); //ForDemonstration
 // Go to ShoppingCart and check if goods was added
         homePage.clickShoppingCart();
         Assert.assertTrue(gotoProductsListCartComponent().getProductsCartNameList().contains(partialProductName));
         delayExecution(2000); //ForDemonstration
-        
-        //TODO ВИНЕСТИ В ТЕСТРАНЕР "Return to previous state"
-    }
+    }//TODO Логер + репортер
 }
